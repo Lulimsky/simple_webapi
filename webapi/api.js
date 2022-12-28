09e4bc6b80e03a9904705161607ff699ed3d5e9a
lulimsky <3337@holbertonschool.com>
2022-12-28 16:03:57 -0300

    api

--- webapi/api.js
+++ webapi/api.js
@@ -2,6 +2,7 @@ let signup = document.querySelector(".signup");
 let login = document.querySelector(".login");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
+let log = document.getElementById("clkbtn").querySelectorAll(".signup-box");
 
 signup.addEventListener("click", () => {
 	slider.classList.add("moveslider");
@@ -11,4 +12,63 @@ signup.addEventListener("click", () => {
 login.addEventListener("click", () => {
 	slider.classList.remove("moveslider");
 	formSection.classList.remove("form-section-move");
+  //let singup-box = document.querySelector(".userlogin").textContent,
+  //let login-box = document.querySelector(".password").textContent;
 });
+
+//var myHeaders = new Headers();
+//myHeaders.append("Authorization", "Bearer 738CD655-02D3-44FD-9DEE-B2B0AF5E363B");
+//
+//var formdata = new FormData();
+//
+//var requestOptions = {
+//  method: 'POST',
+  //headers: myHeaders,
+  //body: formdata,
+ // redirect: 'follow'
+//};
+
+//fetch("https://webapi-sta.012global.com/api/DevTest/SendVerificationEmail", requestOptions)
+  //.then(response => response.text())
+  //.then(result => console.log(result))
+ // .catch(error => console.log('error', error)); 
+//
+
+const BASE_URL = 'https://webapi-sta.012global.com/api/DevTest';
+const AGENT_TOKEN = "738CD655-02D3-44FD-9DEE-B2B0AF5E363B";
+
+// Declaración de la funcón
+// Parametros por posición.
+const post = async (endpoint, body) => {
+  const requestOptions = {
+    method: 'POST',
+    body: JSON.stringify(body),
+  };
+
+  return fetch(`${BASE_URL}${endpoint}`, requestOptions);
+};
+
+const test = async (email, password) => {
+    const signUpResponse =  await post('/UserLogin', {
+  "AgentToken": AGENT_TOKEN,
+  "Email": emailele,
+  "Password": passwordele,
+})
+    
+    console.log('[API-STATUS]', signUpResponse.status)
+    
+    const signUpJson = await signUpResponse.json()
+    
+    console.log('[SIGN UP]', signUpJson)
+  
+  const response =  await post('/SendVerificationEmail', {
+  "AgentToken": AGENT_TOKEN,
+  "Email": emailele
+})
+
+  const json = await response.json()
+  
+  console.log(json)
+}
+
+test("lucila.mociulsky@holbertonstudents.com")
